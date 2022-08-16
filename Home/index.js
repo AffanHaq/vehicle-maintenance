c4cd7eb505254aa52934b1d2ff2064c92cd44bf5
AffanHaq <affanwaseemhaq@gmail.com>
2022-08-17 03:03:23 +0500

    Added Contact form, Booking Form, Call Form, footer, navigation and sripting also done for service panes

--- Home/index.js
+++ Home/index.js
@@ -1,13 +1,5 @@
-// Basket display
-const menuItems = document.querySelectorAll(".menu-item");
-const cartButton = document.querySelector('.btn-cart');
-const cartList = document.querySelector('.basket');
-const closeBasketicons = document.querySelector('#closeBasketicon');
-
 // Modal
-const carModalButton = document.querySelector('.car-svg');
-const bikeModalButton = document.querySelector('.bike-svg');
-const truckModalButton = document.querySelector('.truck-svg');
+
 const CarModal =document.querySelector('.car-modal');
 const bikeModal =document.querySelector('.bike-modal');
 const truckModal =document.querySelector('.truck-modal');
@@ -25,15 +17,92 @@ const Menu4 = document.querySelector('.car-oil');
 const Menu5 = document.querySelector('.car-wash');
 const Menu6 = document.querySelector('.car-inspection');
 
-// Basket Button on-click
+// NEW VARIABLES FOR MODALS
+const appointmentModal = document.querySelector('.book-appointment');
+const exitButtonAppointment = document.querySelector('#closeAppointment');
 
-const closeList = () => {
-        cartList.style.display = 'none';
+const exitGetCall = document.querySelector('#exitCall');
+const openGetCall = document.querySelector('.getacall-icon');
+const modalGetCall = document.querySelector('.getacall');
+
+
+const bookAppointmentTruck = document.querySelector('#bookAppointmentTruck');
+const bookAppointmentCar = document.querySelector('#bookAppointmentCar');
+const bookAppointmentBike = document.querySelector('#bookAppointmentBike');
+const vehicleTypePlaceHolder = document.querySelector('#appoint-vehicle-type');
+
+const carServiceModalOpenner = document.querySelector('#openServiceCardCar');
+const bikeServiceModalOpenner = document.querySelector('#openServiceCardBike');
+const truckServiceModalOpenner = document.querySelector('#openServiceCardTruck');
+
+
+// BOOK APPOINTMENT
+const OpenAppointmnet = () =>{
+    appointmentModal.style.display = 'grid';
+
 }
 
-const openList = () =>{
-    cartList.style.display = 'flex';
+const closeAppointmentModal = () =>{
+    appointmentModal.style.display = 'none';
 }
+
+bookAppointmentBike.addEventListener('click',() =>{
+    vehicleTypePlaceHolder.placeholder = 'VEHICLE TYPE: BIKE';
+    OpenAppointmnet();
+});
+bookAppointmentCar.addEventListener('click', () =>{
+    vehicleTypePlaceHolder.placeholder = 'VEHICLE TYPE: CAR';
+    OpenAppointmnet();
+});
+bookAppointmentTruck.addEventListener('click', () =>{
+    vehicleTypePlaceHolder.placeholder = 'VEHICLE TYPE: TRUCK';
+    OpenAppointmnet();
+});
+exitButtonAppointment.addEventListener('click', closeAppointmentModal)
+
+// VEHICLE SERVICE MODAL
+
+const openModal = () =>{
+    CarModal.style.display = 'grid';
+}
+
+const closeSeriveModals = (e) => {
+    if(e.target.classList.contains('car-modal')){
+        CarModal.style.display = 'none';
+    }
+    else if(e.target.classList.contains('bike-modal')){
+        bikeModal.style.display = 'none';
+    }
+    else if(e.target.classList.contains('truck-modal')){
+
+        truckModal.style.display = 'none';
+    }
+}
+
+// GET A CALL 
+const openCallModal = () =>{
+    modalGetCall.style.display = 'grid';
+
+}
+
+const closeCallModal = () =>{
+    modalGetCall.style.display = 'none';
+}
+
+openGetCall.addEventListener('click', openCallModal);
+exitGetCall.addEventListener('click', closeCallModal);
+
+carServiceModalOpenner.addEventListener('click', openModal);
+CarModal.addEventListener('click', closeSeriveModals);
+bikeModal.addEventListener('click', closeSeriveModals);
+truckModal.addEventListener('click', closeSeriveModals);
+
+bikeServiceModalOpenner.addEventListener('click', () => {
+    bikeModal.style.display = 'grid';
+});
+truckServiceModalOpenner.addEventListener('click', () => {
+    truckModal.style.display = 'grid';
+});
 
 cartButton.addEventListener('click', openList);
 closeBasketicon.addEventListener('click', closeList)
@@ -91,44 +160,8 @@ Menu4.addEventListener('click', oilActive);
 Menu5.addEventListener('click', washActive);
 Menu6.addEventListener('click', inspectActive);
 
-// Modal OPEN AND EXIT
 
-const openModal = () =>{
-    CarModal.style.display = 'grid';
-}
 
-const closecarModal = (e) => {
-    if(e.target.classList.contains('car-modal')){
-        CarModal.style.display = 'none';
-    }
-}
 
-const openBikeModal = () =>{
-    bikeModal.style.display = 'grid';
-}
 
-const closeBikeModal = (e) => {
-    if(e.target.classList.contains('bike-modal')){
-        bikeModal.style.display = 'none';
-    }
-}
-
-const openTruckModal = () =>{
-    truckModal.style.display = 'grid';
-}
-
-const closeTruckModal = (e) => {
-    if(e.target.classList.contains('truck-modal')){
-        truckModal.style.display = 'none';
-    }
-}
-
-carModalButton.addEventListener('click', openModal);
-CarModal.addEventListener('click', closecarModal);
-
-bikeModalButton.addEventListener('click', openBikeModal);
-bikeModal.addEventListener('click', closeBikeModal);
-
-truckModalButton.addEventListener('click', openTruckModal);
-truckModal.addEventListener('click', closeTruckModal);
 
